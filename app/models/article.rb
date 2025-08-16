598a6a6a4699cb3d24be51ffa74bd3c1bf992d51
Saif Wael <saifwael015@gmail.com>
2025-08-17 01:26:17 +0300

    Blog project

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+    include Visible
+
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
+
+    has_many :comments, dependent: :destroy
+end
---
+++ db/migrate/20250816212826_add_status_to_article.rb
@@ -0,0 +1,5 @@
+class AddStatusToArticle < ActiveRecord::Migration[8.0]
+  def change
+    add_column :articles, :status, :string
+  end
+end
